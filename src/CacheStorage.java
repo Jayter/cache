e7229f65bc6d6bc38977ba243c8e778a3f6a212f
Zhenya <kompovich@gmail.com>
2016-04-06 21:10:50 +0300

    Update CacheStorage.java

--- src/CacheStorage.java
+++ src/CacheStorage.java
@@ -1,37 +1,48 @@
-import java.util.HashMap;
-import java.util.Map;
+import gov.nasa.jpf.util.OATHash;
 
+import java.util.HashMap;
 /**
  * Created by Nicki on 04.04.2016.
  */
 public class CacheStorage {
 
-    private static Map<StorageKey, Object> storage = new HashMap<>();
+    private static HashMap<Integer, Object> storage = new HashMap<>();
+
+    //set to config.getBoolean("nhandler.cache”) during initialization!
+    public static boolean toBeCached;
 
     public static void putEntry(Object caller, Object result, Object... args)
     {
-        storage.put(new StorageKey(caller, args), result);
+        storage.put(getHashCode(caller, args), result);
     }
 
-    public static boolean containsEntry(Object caller, Object result, Object... args)
+    public static boolean containsEntry(Object caller, Object... args)
     {
-        return storage.containsKey(new StorageKey(caller, args));
-    }
-
-    /**
-     * Checks whether the method is specified to be cached.
-     * @param caller
-     * @param result
-     * @param args
-     * @return true if certain method needs to be cached or false otherwise
-     */
-    public static boolean toBeCached(Object caller, Object result, Object... args)
-    {
-        return config.getBoolean("nhandler.cache");
+        return storage.containsKey(getHashCode(caller, args));
     }
 
     public Object getResult(Object caller, Object... args)
     {
-        return storage.get(new StorageKey(caller, args));
+        return storage.get(getHashCode(caller, args));
+    }
+
+    //calculates and returns the hash value of the given parameters
+    private static int getHashCode(Object caller, Object... args)
+    {
+        int hash;
+        if(args != null && args.length != 0) {
+            int[] hashCodes = new int[args.length + 1];
+            hashCodes[0] = caller != null ? caller.hashCode() : 0;
+            for (int i = 1; i < hashCodes.length; i++) {
+                Object arg = args[i - 1];
+                hashCodes[i] = arg != null ? arg.hashCode() : 0;
+            }
+            return OATHash.hash(hashCodes);
+        }
+        else
+        {
+            hash = caller != null ? caller.hashCode() : 0;
+            return OATHash.hash(hash);
+        }
     }
 }
