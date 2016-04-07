4fb5731dcec4a234e46c3ce842339b3db997a13f
Zhenya <kompovich@gmail.com>
2016-04-07 23:50:11 +0300

    Update CacheStorage.java

--- src/CacheStorage.java
+++ src/CacheStorage.java
@@ -6,64 +6,64 @@ import java.util.HashMap;
  */
 public class CacheStorage {
 
-    private static HashMap<Integer, Object> storage = new HashMap<>();
+  private static HashMap<Integer, Object> storage = new HashMap<>();
 
-    //set to config.getBoolean("nhandler.cache”) during initialization!
-    public static boolean toBeCached;
+  //set to config.getBoolean("nhandler.cache”) during initialization!
+  public static boolean toBeCached;
 
-    public static void putEntry(Object caller, Object result, Object... args) {
-      storage.put(getHashCode(caller, args), result);
-    }
+  public static void putEntry(Object caller, Object result, Object... args) {
+    storage.put(getHashCode(caller, args), result);
+  }
+ 
+  public static boolean containsEntry(Object caller, Object... args) {
+    return storage.containsKey(getHashCode(caller, args));
+  }
 
-    public static boolean containsEntry(Object caller, Object... args) {
-      return storage.containsKey(getHashCode(caller, args));
-    }
-
-    public Object getResult(Object caller, Object... args) {
-      return storage.get(getHashCode(caller, args));
-    }
+  public Object getResult(Object caller, Object... args) {
+    return storage.get(getHashCode(caller, args));
+  }
     
-    public static Object invokeDelegatedCall(Method method, Object caller, Object... args) {
-      if(!toBeCached) {
-        try {
-          return method.invoke(caller, args);
-        } 
-        catch (IllegalAccessException | InvocationTargetException e) {
-          e.printStackTrace();
-          return null;
-        }
-      }
-      if(containsEntry(caller, args)) {
-        return getResult(caller, args);
-      }
-      else {
-        try {
-          Object returnVal = method.invoke(caller, args);
-          putEntry(caller, returnVal, args);
-          return returnVal;
-        }
-        catch (IllegalAccessException | InvocationTargetException e) {
-          e.printStackTrace();
-          return null;
-        }
+  public static Object invokeDelegatedCall(Method method, Object caller, Object... args) {
+    if(!toBeCached) {
+      try {
+        return method.invoke(caller, args);
+      } 
+      catch (IllegalAccessException | InvocationTargetException e) {
+        e.printStackTrace();
+        return null;
       }
     }
+    if(containsEntry(caller, args)) {
+      return getResult(caller, args);
+    }
+    else {
+      try {
+        Object returnVal = method.invoke(caller, args);
+        putEntry(caller, returnVal, args);
+        return returnVal;
+      }
+      catch (IllegalAccessException | InvocationTargetException e) {
+        e.printStackTrace();
+        return null;
+      }
+    }
+  }
     
-    //calculates and returns the hash value of the given parameters
-    private static int getHashCode(Object caller, Object... args) {
-      int hash;
-      if(args != null && args.length != 0) {
-        int[] hashCodes = new int[args.length + 1];
-        hashCodes[0] = caller != null ? caller.hashCode() : 0;
-        for (int i = 1; i < hashCodes.length; i++) {
-        Object arg = args[i - 1];
-        hashCodes[i] = arg != null ? arg.hashCode() : 0;
-        }
-        return OATHash.hash(hashCodes);
+  //calculates and returns the hash value of the given parameters
+  private static int getHashCode(Object caller, Object... args) {
+    int hash;
+    if(args != null && args.length != 0) {
+      int[] hashCodes = new int[args.length + 1];
+      hashCodes[0] = caller != null ? caller.hashCode() : 0;
+      for (int i = 1; i < hashCodes.length; i++) {
+      Object arg = args[i - 1];
+      hashCodes[i] = arg != null ? arg.hashCode() : 0;
       }
-      else {
-        hash = caller != null ? caller.hashCode() : 0;
-        return OATHash.hash(hash);
-      }
+      return OATHash.hash(hashCodes);
     }
+    else {
+      hash = caller != null ? caller.hashCode() : 0;
+      return OATHash.hash(hash);
+    }
+  }
 }
